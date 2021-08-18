fcb74aa40325f4030a80b7379d66a5e80ba49322
deiby <deiby.lopez@utp.ac.pa>
2021-08-18 03:55:15 -0500

    Modificaciones

--- src/test/java/stepdefinitons/UtestStepDefinition.java
+++ src/test/java/stepdefinitons/UtestStepDefinition.java
@@ -1,14 +1,11 @@
 package stepdefinitons;
 
-import answer.Answer;
-import cucumber.api.DataTable;
 import cucumber.api.PendingException;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import model.UtestData;
-import net.serenitybdd.screenplay.GivenWhenThen;
 import net.serenitybdd.screenplay.actors.OnStage;
 import net.serenitybdd.screenplay.actors.OnlineCast;
 import tasks.*;
